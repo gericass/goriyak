6e7cf24f92a9ca1ac1d0bc7f45f0b5291640a21b
Keita <bonjovi1926@yahoo.co.jp>
2017-12-29 22:29:32 +0900

    add: data structure

---
+++ model/local/transaction.sql
@@ -0,0 +1,8 @@
+CREATE TABLE transaction (
+  id              BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  send_node_id    BIGINT UNSIGNED NOT NULL,
+  recieve_node_id BIGINT UNSIGNED NOT NULL,
+  amount          DOUBLE          NOT NULL,
+  created_at      DATETIME        NOT NULL
+);
+
