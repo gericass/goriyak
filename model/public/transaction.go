e6567fb5bd355b2095336167b720214aa04f6722
Keita <bonjovi1926@yahoo.co.jp>
2017-12-30 15:27:29 +0900

    add: test

--- model/public/transaction.go
+++ model/public/transaction.go
@@ -10,8 +10,8 @@ import (
 type PublicTransaction struct {
 	ID            string    `json:"id"`
 	Name          string    `json:"name"`
-	SendNodeID    int64     `json:"send_node_id"`
-	ReceiveNodeID int64     `json:"receive_node_id"`
+	SendNodeID    string     `json:"send_node_id"`
+	ReceiveNodeID string     `json:"receive_node_id"`
 	Amount        float64   `json:"amount"`
 	Status        string    `json:"status"`
 	CreatedAt     time.Time `json:"created_at"`
--- model/public/transaction_test.go
+++ model/public/transaction_test.go
@@ -1,1 +1,23 @@
 package public
+
+import (
+	"testing"
+	"time"
+	"strings"
+)
+
+func TestPutTransaction(t *testing.T) {
+	tr := &PublicTransaction{ID: "test",
+		Name: "testNode",
+		SendNodeID: "send",
+		ReceiveNodeID: "receive",
+		Amount: 0.00,
+		Status: "approved",
+		CreatedAt: time.Now(),
+	}
+	actual, _ := tr.PutTransaction()
+	expected := "204"
+	if !strings.Contains(actual, expected) {
+		t.Error("PutTransaction(): expected %s, actual ", expected, actual)
+	}
+}
