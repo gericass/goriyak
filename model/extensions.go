1e7740fe461efaffc5c09cd49f3e1fc55150ac5d
Keita <bonjovi1926@yahoo.co.jp>
2017-12-30 17:13:40 +0900

    fix

--- model/extensions.go
+++ model/extensions.go
@@ -1,1 +1,21 @@
 package model
+
+import (
+	"net/http"
+	"bytes"
+)
+
+func PutRequest(url string, body string) (*http.Response, error) {
+	req, err := http.NewRequest("PUT", url, bytes.NewBuffer([]byte(body)))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
