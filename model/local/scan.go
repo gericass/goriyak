afea40261b601a905a5449072e823f594ecd1c68
Keita <bonjovi1926@yahoo.co.jp>
2018-01-03 00:37:09 +0900

    fix: scan

--- model/local/scan.go
+++ model/local/scan.go
@@ -10,7 +10,7 @@ func scanTransactions(rows *sql.Rows) ([]*LocalTransaction, error) {
 
 	for rows.Next() {
 		t := new(LocalTransaction)
-		if err := rows.Scan(&t.ID, &t.Name, &t.SendNodeID, &t.ReceiveNodeID, &t.Amount, &t.CreatedAt); err != nil {
+		if err := rows.Scan(&t.ID, &t.Name, &t.SendNodeID, &t.ReceiveNodeID, &t.Amount, &t.Status, &t.CreatedAt, &t.UpdatedAt); err != nil {
 			return nil, err
 		}
 		transactions = append(transactions, t)
