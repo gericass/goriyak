2abbcea0e5e2dec116936210d13703edc6808e54
Keita <bonjovi1926@yahoo.co.jp>
2018-01-02 20:21:47 +0900

    fix

--- model/local/sql.go
+++ model/local/sql.go
@@ -37,12 +37,6 @@ func GetTransactionsByName(name string, tx *sql.Tx) ([]*LocalTransaction, error)
 
 // PutTransaction : to put transactions to MySQL transaction table
 func (t *LocalTransaction) PutTransaction(tx *sql.Tx) error {
-	query := "INSERT INTO `transaction` (`id`, `name`, `send_node_id`, `receive_node_id`,`amount`,`created_at`) values(?, ?, ?, ?, ?, ?)"
-	stmt, err := tx.Prepare(query)
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
 	return dbTransaction(tx, func(tx *sql.Tx) error {
 		query := "INSERT INTO `transaction` (`name`, `send_node_id`, `receive_node_id`,`amount`,`created_at`) values(?, ?, ?, ?, ?)"
 		stmt, err := tx.Prepare(query)
