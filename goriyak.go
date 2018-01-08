c4de7c6430831701564ee428b76d159278c9ab2f
Keita <bonjovi1926@yahoo.co.jp>
2018-01-08 22:38:38 +0900

    add: signal handling

--- goriyak.go
+++ goriyak.go
@@ -8,11 +8,31 @@ import (
 	"github.com/gericass/goriyak/application/handler"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gericass/goriyak/model/local"
+	"os"
+	"os/signal"
+	"syscall"
+	"fmt"
 )
 
 const grpcPort = ":50051"
 
 func main() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT)
+	// TODO implements Graceful shutdown
+	go func() {
+		for {
+			s := <-signalChan
+			switch s {
+			// kill -SIGHUP XXXX
+			case syscall.SIGHUP:
+				fmt.Println("hungup")
+				// kill -SIGINT XXXX or Ctrl+c
+			case syscall.SIGINT:
+				fmt.Println("Warikomi")
+			}
+		}
+	}()
 	lis, err := net.Listen("tcp", grpcPort)
 	if err != nil {
 		log.Println("failed to listen: %v", err)
